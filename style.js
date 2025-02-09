1827e8a7912b415ca921fa072b2386fdd9a58168
Abhi5292 <abhisekchhetri10@gmail.com>
2025-02-09 20:51:38 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.getElementById("flash-screen").style.display = "none";
+        document.getElementById("game-container").style.display = "block";
+    }, 3000);
+});
